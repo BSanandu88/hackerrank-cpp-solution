49aa425cca863f3a28dad5b1fea78ad8ec34f3be
BSanandu88 <59107214+BSanandu88@users.noreply.github.com>
2020-05-16 08:57:38 +0530

    Add files via upload

---
+++ classes and object.cpp
@@ -0,0 +1,16 @@
+class Student{
+    int scores[5];
+    public:
+    void input(){
+        for(int i=0; i<5; i++){
+            cin >> scores[i];
+        }
+    }
+    int calculateTotalScore(){
+        int total = 0;
+        for(int i=0; i<5; i++){
+            total += scores[i];
+        }
+        return total;
+    }
+};
