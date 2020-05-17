75a36182ba6f96fd879e5bf2f8d47e6555c848fb
BSanandu88 <59107214+BSanandu88@users.noreply.github.com>
2020-05-17 12:35:34 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,67 +1,10 @@
-#include <iostream>
-#include <sstream>
-using namespace std;
-
-class Student {
-    private:
-        int age;
-        string first_name;
-        string last_name;
-        int standard;
-    public:
-        void set_age(int a) {
-            age = a;
-        }
-        int get_age() {
-            return age;
-        }
-
-        void set_first_name(string fn) {
-            first_name = fn;
-        }
-        string get_first_name() {
-            return first_name;
-        }
-
-        void set_last_name(string ln) {
-            last_name = ln;
-        }
-        string get_last_name() {
-            return last_name;
-        }
-
-        void set_standard(int s) {
-            standard = s;
-        }
-        int get_standard() {
-            return standard;
-        }
-
-        string to_string() {
-            stringstream ss;
-            ss << age << "," << first_name << "," << last_name << "," << standard;
-            return ss.str();
-        }
-};
-
-
-int main() {
-    int age, standard;
-    string first_name, last_name;
-
-    cin >> age >> first_name >> last_name >> standard;
-
-    Student st;
-    st.set_age(age);
-    st.set_standard(standard);
-    st.set_first_name(first_name);
-    st.set_last_name(last_name);
-
-    cout << st.get_age() << "\n";
-    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
-    cout << st.get_standard() << "\n";
-    cout << "\n";
-    cout << st.to_string();
-
-    return 0;
+try{
+	int x=Server::compute(A,B);
+	cout<<x<<endl;
+}catch(bad_alloc& e){
+	cout<<"Not enough memory"<<'\n';
+}catch(exception &e){
+	cout<<"Exception: "<<e.what()<<'\n';
+}catch(...){
+	cout<<"Other Exception"<<'\n';
 }
